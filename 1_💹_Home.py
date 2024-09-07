4d1e1d5edb0ddf00f1735e4fee6a8a94063a5ba6
Ahmed Fawzy <ahmed.mohamed.fawzy.abdelfattah@gmail.com>
2024-09-07 15:07:34 +0300

    LFS

---
+++ 1_💹_Home.py
@@ -0,0 +1,100 @@
+# import libraries
+import streamlit as st
+from streamlit_lottie import st_lottie
+import json
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+
+st.set_page_config(
+    page_title="Home",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon="💹",
+)
+
+st.title("Deposit Decision Predictor")
+
+# Load data
+df = pd.read_csv("cleaned_data.csv")
+
+
+# sidebar
+st.sidebar.header("Deposit Decision Predictor")
+st.sidebar.image("home.jpeg")
+st.sidebar.markdown(
+    "Made By : [Ahmed Fawzy](https://www.linkedin.com/in/ahmedfawzy-ko/)"
+)
+
+# Body
+
+# Animation upload and open
+with open("animation.json") as source:
+    animation = json.load(source)
+st_lottie(animation)
+
+# About Data section
+st.title("Bank Marketing Dataset Overview")
+
+# Dataset Overview
+st.header("Dataset Overview")
+st.write("""
+- The dataset comes from the UCI Machine Learning Repository and contains information on direct marketing campaigns by a Portuguese banking institution.
+""")
+
+# Main Objective
+st.header("Main Objective")
+st.write("""
+- Predict the outcome (yes or no) of whether a customer will subscribe to a term deposit.
+""")
+
+# Features
+st.header("Features")
+
+st.subheader("Client Information")
+st.write("""
+- **age**: Age of the client (numeric).
+- **job**: Type of job (categorical: admin., blue-collar, entrepreneur, etc.).
+- **marital**: Marital status (categorical: single, married, divorced).
+- **education**: Education level (categorical: primary, secondary, tertiary, unknown).
+- **default**: Has credit in default? (categorical: yes, no).
+- **housing**: Has a housing loan? (categorical: yes, no).
+- **loan**: Has a personal loan? (categorical: yes, no).
+""")
+
+st.subheader("Contact Information")
+st.write("""
+- **contact**: Contact communication type (categorical: cellular, telephone).
+- **month**: Last contact month of year (categorical: jan, feb, mar, ..., dec).
+- **day_of_week**: Last contact day of the week (categorical: mon, tue, wed, thu, fri).
+- **duration**: Last contact duration in seconds (numeric).
+""")
+
+st.subheader("Campaign Information")
+st.write("""
+- **campaign**: Number of contacts performed during this campaign (numeric).
+- **pdays**: Days since the client was last contacted from a previous campaign (numeric; 999 indicates not previously contacted).
+- **previous**: Number of contacts performed before this campaign (numeric).
+- **poutcome**: Outcome of the previous marketing campaign (categorical: failure, nonexistent, success).
+""")
+
+st.subheader("Economic Context")
+st.write("""
+- **emp.var.rate**: Employment variation rate (numeric).
+- **cons.price.idx**: Consumer price index (numeric).
+- **cons.conf.idx**: Consumer confidence index (numeric).
+- **euribor3m**: Euribor 3-month rate (numeric).
+- **nr.employed**: Number of employees (numeric).
+""")
+
+# Target Variable
+st.header("Target Variable")
+st.write("""
+- **y**: Indicates if the client subscribed to a term deposit (binary: yes or no).
+""")
+
+
+btn = st.button("Show a random sample from the final dataset")
+if btn:
+    st.write(df.sample(5))
---
+++ pages/3_📞_predictor.py
@@ -0,0 +1,143 @@
+
+# import libraries
+import plotly.graph_objects as go
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle as pkl
+from streamlit_lottie import st_lottie
+import json
+
+st.set_page_config(
+    page_title="Term Deposit Success Predictor",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    page_icon="📊",
+)
+
+st.title("Term Deposit Success Predictor")
+
+# Body
+
+# Animation upload and open
+with open("prediction.json") as source:
+    animation = json.load(source)
+st_lottie(animation)
+
+# sidebar
+
+st.sidebar.header("Term Deposit Success Predictor")
+st.sidebar.image("data_analysis.jpeg")
+st.sidebar.subheader("Choose Your Favorite Predictor")
+
+# Filters
+model = st.sidebar.selectbox(
+    "predictors", ["Random Forest", "Gradient Boost","XG Boost"]
+)
+
+# Load Cleaned Data
+df = pd.read_csv('Cleaned_data.csv')
+
+# Load preprocessor
+preprocessor = pkl.load(open('preprocessor.pkl', 'rb'))
+
+# Load models
+rf = pkl.load(open('rf.pkl', 'rb'))
+gb = pkl.load(open('gb.pkl', 'rb'))
+xgb = pkl.load(open('xgb.pkl', 'rb'))
+
+# model selection
+if model=="Random Forest":
+    model=rf
+elif model=="Gradient Boost":
+    model=gb
+elif model=="XG Boost":
+    model=xgb
+
+# Input Data
+
+# Create two columns
+col1, col2 = st.columns(2)
+
+# First column inputs
+with col1:
+    job = st.selectbox('job', df['job'].unique())
+    marital = st.selectbox('marital', df['marital'].unique())
+    education = st.selectbox('education', df['education'].unique())
+    housing = st.selectbox('housing', df['housing'].unique())
+    loan = st.selectbox('loan', df['loan'].unique())
+    contact = st.selectbox('contact', df['contact'].unique())
+    month = st.selectbox('month', df['month'].unique())
+    day_of_week = st.selectbox('day_of_week', df['day_of_week'].unique())
+    contacted_before = st.selectbox('contacted_before', df['contacted_before'].unique())
+    poutcome = st.selectbox('poutcome', df['poutcome'].unique())
+
+
+# Second column inputs
+with col2:
+    age = st.number_input('age', df.age.min(), df.age.max())
+    duration = st.number_input('duration', df.duration.min(), df.duration.max())
+    campaign = st.number_input('campaign', df.campaign.min(), df.campaign.max())
+    previous = st.number_input('previous', df.previous.min(), df.previous.max())
+    emp_var_rate = st.number_input('emp_var_rate', df['emp_var_rate'].min(), df['emp_var_rate'].max())
+    cons_price_idx = st.number_input('cons_price_idx', df['cons_price_idx'].min(), df['cons_price_idx'].max())
+    cons_conf_idx = st.number_input('cons_conf_idx', df['cons_conf_idx'].min(), df['cons_conf_idx'].max())
+    euribor3m = st.number_input('euribor3m', df['euribor3m'].min(), df['euribor3m'].max())
+    nr_employed = st.number_input('nr_employed', df['nr_employed'].min(), df['nr_employed'].max())
+
+# Preprocessing
+new_data = {'job':job,
+            'marital':marital,
+            'education':education,
+            'housing':housing,
+            'loan':loan,
+            'contact':contact,
+            'month':month,
+            'day_of_week':day_of_week,
+            'contacted_before':contacted_before,
+            'age':age,
+            'duration':duration,
+            'campaign':campaign,
+            'previous':previous,
+            'poutcome':poutcome,
+            'emp_var_rate':emp_var_rate,
+            'cons_price_idx':cons_price_idx,
+            'cons_conf_idx':cons_conf_idx,
+            'euribor3m':euribor3m,
+            'nr_employed':nr_employed,
+            }
+
+new_data = pd.DataFrame(new_data, index=[0])
+
+# Preprocessing
+new_data = preprocessor.transform(new_data)
+
+# Prediction
+predection = model.predict(new_data) 
+
+if predection == 0:
+    predection = 'NO'
+else:
+    predection = 'YES'
+
+# Output
+if st.button('Predict'):
+    st.markdown('# Deposit Prediction')
+    st.markdown(predection)
+
+f1_scores=[60.0, 60.0, 59.0]
+recall_scores=[95.0, 94.0, 94.0]
+precision_scores=[44.0, 44.0, 43.0]
+
+fig = go.Figure(data=[go.Table(header=dict(values=['<b>Model<b>', '<b>F1 Score<b>', '<b>Recall<b>', '<b>Precision<b>'],
+                                           line_color='darkslategray',
+    fill_color='whitesmoke',
+    align=['center','center'],
+    font=dict(color='black', size=18),
+    height=40),
+                               
+                 cells=dict(values=[['<b>Random Forest<b>', '<b>AdaBoost<b>','<b>SVM<b>'], [f1_scores[0],f1_scores[1],f1_scores[2]], [recall_scores[0],recall_scores[1],recall_scores[2]], [precision_scores[0],precision_scores[1],precision_scores[2]]]))
+                     ])
+
+fig.update_layout(title='Model Results On Test Data')
+st.plotly_chart(fig, use_container_width=True)
