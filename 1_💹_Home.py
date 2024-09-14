edace8dbbdc4f5e8bf86e2c50d267304586647d7
Ahmed Fawzy <148709604+Ahmed-Mohamed-Fawzy@users.noreply.github.com>
2024-09-14 12:36:23 +0300

    Update 1_💹_Home.py

--- 1_💹_Home.py
+++ 1_💹_Home.py
@@ -16,7 +16,7 @@ st.set_page_config(
 st.title("Deposit Decision Predictor")
 
 # Load data
-df = pd.read_csv(r"datasets\cleaned_data.csv")
+df = pd.read_csv("datasets/cleaned_data.csv")
 
 
 # sidebar
